52639b8047cd2c98738570c9e1e9cac4d0f87e30
Otaku Sensei <61647038+0takusensei@users.noreply.github.com>
2023-10-04 18:36:53 +0600

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,20 @@
+Config = {}
+
+Config['PoleDance'] = {
+    ['Enabled'] = true,
+    ['Locations'] = {
+        {['Position'] = vector3(-565.55, 214.36, 70.54), ['Number'] = '1'},
+   --     {['Position'] = vector3(-565.4, 214.38, 70.54), ['Number'] = '1'},
+   --     {['Position'] = vector3(-565.6, 206.94, 70.76), ['Number'] = '1'},
+     --   {['Position'] = vector3(-571.69, 202.45, 74.68), ['Number'] = '1'},
+
+    }
+}
+
+Strings = {
+    ['Pole_Dance'] = '~b~E',
+}
+
+
+
+---- Uncomment the position to add more loction and change the vector3 coords within -----
